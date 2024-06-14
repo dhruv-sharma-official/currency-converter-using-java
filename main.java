f0de2845c3995994ac280b945418291f5bb8ccf3
Dilip Singh Naruka <dsnaruka995082@gmail.com>
2024-06-14 09:49:05 +0530

    adding switch and changing data types

--- main.java
+++ main.java
@@ -1,42 +1,49 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
-class currency{
-	public double ruppee(double amount){
+class currency {
+	public double ruppee(double amount) {
 		return amount * 83;
 	}
-	public double dollar(double amount){
+
+	public double dollar(double amount) {
 		return amount / 83;
 	}
 }
 
-public class main{
+public class main {
 
-	public static void main(String[] args){
-	boolean a = true;
-	Scanner scanner = new Scanner(System.in);
-	currency convert = new currency();
-	while (a) {
-		System.out.println("\n############ Currency Converter ############ \n1 -> to convert ruppee in dollar\n2 -> to convert dollar in ruppee\n0 -> to exit\n");
-		int inp = scanner.nextInt();
-		System.out.println("\nEnter amount:");
-		double amnt = scanner.nextInt();
+	public static void main(String[] args) {
+		DecimalFormat df = new DecimalFormat("#.00");
+		Scanner scanner = new Scanner(System.in);
+		currency convert = new currency();
+		while (true) {
+			System.out.println(
+					"\n############ Currency Converter ############ \n1 -> to convert ruppee in dollar\n2 -> to convert dollar in ruppee\n0 -> to exit\n");
+			int inp = scanner.nextInt();
+			double amnt;
+			switch (inp) {
+				case 1:
+					System.out.println("\nEnter amount:");
+					amnt = scanner.nextDouble();
+					System.out.println("ruppees " + amnt + " equals to " + df.format(
+							convert.dollar(amnt)) + " in dollar");
+					break;
+				case 2:
+					System.out.println("\nEnter amount:");
+					amnt = scanner.nextDouble();
+					System.out.println("dollar " + amnt + " equals to " + df.format(
+							convert.dollar(amnt)) + " in ruppees");
+					break;
+				case 0:
+					break;
 
-		if (inp == 1) {
-			System.out.println("ruppees "+amnt+" equals to "+convert.dollar(amnt)+" in dollar");
-		
-		} else if (inp == 2) {
-			System.out.println("dollar "+amnt+" equals to "+convert.ruppee(amnt)+" in ruppees");
-			
-		
-		} else if (inp == 0) {
-			System.out.println("exiting");
-			a = false;
+			}
+			if (inp == 0) {
+				break;
+			}
 
-		} else {
-			System.out.println("invalid input");
 		}
-	}
-		
 	}
 
 }
